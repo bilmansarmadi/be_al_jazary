7f76bb302ad19f49667e4b6086672499935f3228
bilmansarmadi <bilmanbudilhur@gmail.com>
2023-03-10 01:33:46 +0700

    vercel

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 import express from 'express'
  const app = express();
-
+ var menuRouter                = require('./api/Master/Menu/Menu');
+ 
  app.use("/", (req,res) => {
      res.send('kayapalah biar konek')
  })
+
+ app.use('/api/Master/Menu', menuRouter);
 
 
  app.listen(5000, ()=> {
