b62dcbaa32bf2700f78120213c8fa21f2f965850
bilmansarmadi <bilmanbudilhur@gmail.com>
2023-03-15 00:07:31 +0700

    index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var logger = require('morgan');
 
 var indexRouter               = require('./routes/index');
 var absensiRouter             = require('./api/Transaction/Absensi/Absensi');
-var kelompokkelasRouter       = require('./api/Transaction/Absensi/Absensi');
+var kelompokkelasRouter       = require('./api/Transaction/Kelompok_Kelas/Kelompok_Kelas');
 var rpendidikanRouter         = require('./api/Transaction/Riwayat_Pendidikan_Santri/Riwayat_Pendidikan_Santri');
 var rdikpengajarRouter        = require('./api/Transaction/Riwayat_Pendidikan_Pengajar/Riwayat_Pendidikan_Pengajar');
 var rmengajarRouter           = require('./api/Transaction/Riwayat_Mengajar/Riwayat_Mengajar');
@@ -25,54 +25,54 @@ var santriRouter              = require('./api/Master/Santri/Santri');
 var pengajaranRouter          = require('./api/Master/Pengajaran/Pengajaran');
 var rprivilegeRouter          = require('./api/Master/Rprivilege/Rprivilege');
 
-var index = express();
+var app = express();
 
 // view engine setup
-index.set('views', path.join(__dirname, 'views'));
-index.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
-index.use(logger('dev'));
-index.use(express.json());
-index.use(express.urlencoded({ extended: false }));
-index.use(cookieParser());
-index.use('/api/', express.static(path.join(__dirname, 'public')));
-index.use('/api/', indexRouter);
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use('/api/', express.static(path.join(__dirname, 'public')));
+app.use('/api/', indexRouter);
 
-index.use('/api/Riwayat/Absensi', absensiRouter);
-index.use('/api/Riwayat/Riwayat_Pendidikan_Santri', rpendidikanRouter);
-index.use('/api/Riwayat/Riwayat_Pendidikan_Pengajar', rdikpengajarRouter);
-index.use('/api/Riwayat/Riwayat_Nilai', riwayatnilaiRouter);
-index.use('/api/Riwayat/Riwayat_Mengajar', rmengajarRouter);
-index.use('/api/Riwayat/Kelompok_Kelas', kelompokkelasRouter);
-index.use('/api/Master/Users', usersRouter);
-index.use('/api/Master/RoleUser', roleUserRouter);
-index.use('/api/Master/TahunAjaran', tahunajaranRouter);
-index.use('/api/Master/Kelas', kelasRouter);
-index.use('/api/Master/Menu', menuRouter);
-index.use('/api/Master/Pengajar', pengajarRouter);
-index.use('/api/Master/Santri', santriRouter);
-index.use('/api/Master/Mapel', mapelRouter);
-index.use('/api/Master/GradeNilai', gradenilaiRouter);
-index.use('/api/Master/Pengajaran', pengajaranRouter);
-index.use('/api/Master/Privilege', rprivilegeRouter);
-index.use('/api/Master/Kurikulum', kurikulumRouter);
-index.use('/api/Master/Kategori', kategoriRouter);
+app.use('/api/Riwayat/Absensi', absensiRouter);
+app.use('/api/Riwayat/Riwayat_Pendidikan_Santri', rpendidikanRouter);
+app.use('/api/Riwayat/Riwayat_Pendidikan_Pengajar', rdikpengajarRouter);
+app.use('/api/Riwayat/Riwayat_Nilai', riwayatnilaiRouter);
+app.use('/api/Riwayat/Riwayat_Mengajar', rmengajarRouter);
+app.use('/api/Riwayat/Kelompok_Kelas', kelompokkelasRouter);
+app.use('/api/Master/Users', usersRouter);
+app.use('/api/Master/RoleUser', roleUserRouter);
+app.use('/api/Master/TahunAjaran', tahunajaranRouter);
+app.use('/api/Master/Kelas', kelasRouter);
+app.use('/api/Master/Menu', menuRouter);
+app.use('/api/Master/Pengajar', pengajarRouter);
+app.use('/api/Master/Santri', santriRouter);
+app.use('/api/Master/Mapel', mapelRouter);
+app.use('/api/Master/GradeNilai', gradenilaiRouter);
+app.use('/api/Master/Pengajaran', pengajaranRouter);
+app.use('/api/Master/Privilege', rprivilegeRouter);
+app.use('/api/Master/Kurikulum', kurikulumRouter);
+app.use('/api/Master/Kategori', kategoriRouter);
 
 
 // catch 404 and forward to error handler
-index.use(function(req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-index.use(function(err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.index.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
 
-module.exports = index;
+module.exports = app;
